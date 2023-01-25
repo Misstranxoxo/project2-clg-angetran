0d86f558f8d318f1c8c89f39c77b90c07bdae822
Ange Tran <ange.tran@health.vic.gov.au>
2023-01-25 14:20:54 +1100

    Updating Project

--- project2-clg-angetran/js/main.js
+++ project2-clg-angetran/js/main.js
@@ -1,78 +1,117 @@
-function angesWorld() {
-    alert("Welcome to my world!");
+var menuToggle = document.querySelector("#menu-toggle");
+var activeElements = document.querySelectorAll(".active-element");
+var toggledMenu = menuToggle.addEventListener("click", function(){
+     // forEach is not supported in IE11
+  // activeElements.forEach(function(e){
+  //     e.classList.toggle("active");
+  // });
+     for(var activated = 0; activated < activeElements.length; activated++){
+          activeElements[activated].classList.toggle("active");
+     }
+})
+
+// Happy bouncing ball background
+console.log("Bouncing Ball Fun");
+
+var canvas = document.getElementById("canvas");
+var c = canvas.getContext("2d");
+var tx = window.innerWidth;
+var ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
+//c.lineWidth= 5;
+//c.globalAlpha = 0.5;
+
+var mousex = 0;
+var mousey = 0;
+
+addEventListener("mousemove", function() {
+  mousex = event.clientX;
+  mousey = event.clientY;
+});
+
+
+var grav = 0.99;
+c.strokeWidth=5;
+function randomColor() {
+  return (
+    "rgba(" +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.ceil(Math.random() * 10) / 10 +
+    ")"
+  );
 }
-angesWorld();
-//this is an alert for when the angesWorld function is called
 
-favCharacter (" Hello Kitty");
-function favCharacter(name) {
-    console.log ("My favourite character of all time is" + name);
+function Ball() {
+  this.color = randomColor();
+  this.radius = Math.random() * 20 + 14;
+  this.startradius = this.radius;
+  this.x = Math.random() * (tx - this.radius * 2) + this.radius;
+  this.y = Math.random() * (ty - this.radius);
+  this.dy = Math.random() * 2;
+  this.dx = Math.round((Math.random() - 0.5) * 10);
+  this.vel = Math.random() /5;
+  this.update = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    c.fillStyle = this.color;
+    c.fill();
+    //c.stroke();
+  };
 }
-// Hoisting= set a function and call them wherever they need to be executed without worrying that the function declaration won't be read
 
-let firstPrize = 1000;
-const departmName = "DH";
-let greeting ="Congratulations!";
-let morning = true;
+var bal = [];
+for (var i=0; i<50; i++){
+    bal.push(new Ball());
+}
 
-console.log(morning);
-console.log(firstPrize);
-
-let firstWinner="Steve";
-console.log ( "the winner is "+ firstWinner);
-console.log (`the winner is still ${firstWinner}`);
-
-const sentence = 'The quick brown fox jumps over the lazy dog.';
-console.log(sentence.toUpperCase());
-console.log (sentence.length)
-console.log(`The length of the sentence is ${sentence.length}`);
-// expected output: "the quick brown fox jumps over the lazy dog."
-// testing variables & data types
-
-console.log (10*7);
-console.log (15/3);
-//testing arthmetic operators
-let attendee = 1;
-console.log (attendee ++);
-console.log (attendee ++);
-console.log (attendee ++);
-//testing decrement operators 
-
-typeof attendee;
-console.log(typeof attendee)
-let attendeeName ="Ange";
-console.log(typeof attendeeName);
-// to know what type of data we are working with, e.g text = string
-
-let James = 42;
-let Ange = 36;
-//defining variables
-
-console.log (James >=37 && James <=55);
-console.log (Ange >=20 && Ange <=35);
-// expressions which return different values to console depending on conditions
-
-let age = 36;
-const schoolOfCode = (age >18)? "You're in!" : "Sorry your're out";
-console.log (schoolOfCode);
-// condition ? ifTrue: ifFalse
-// testing ternary operator
-
-let discount = 10;
-const tee = 20;
-const pants = 30;
-const candle = 50;
-
-function shoppingSale( candle, pants) {
-    candle = 20;
-    pants = 20;
-    let shoppingCart = tee + pants + candle; 
-    if (shoppingCart <= 60) {
-        console.log ("you need to spend more");
-        return;
+function animate() {    
+  if (tx != window.innerWidth || ty != window.innerHeight) {
+    tx = window.innerWidth;
+    ty = window.innerHeight;
+    canvas.width = tx;
+    canvas.height = ty;
+  }
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, tx, ty);
+  for (var i = 0; i < bal.length; i++) {
+    bal[i].update();
+    bal[i].y += bal[i].dy;
+    bal[i].x += bal[i].dx;
+    if (bal[i].y + bal[i].radius >= ty) {
+      bal[i].dy = -bal[i].dy * grav;
     } else {
-            console.log(shoppingCart -= discount);        
+      bal[i].dy += bal[i].vel;
+    }    
+    if(bal[i].x + bal[i].radius > tx || bal[i].x - bal[i].radius < 0){
+        bal[i].dx = -bal[i].dx;
     }
+    if(mousex > bal[i].x - 20 && 
+      mousex < bal[i].x + 20 &&
+      mousey > bal[i].y -50 &&
+      mousey < bal[i].y +50 &&
+      bal[i].radius < 70){
+        //bal[i].x += +1;
+        bal[i].radius +=5; 
+      } else {
+        if(bal[i].radius > bal[i].startradius){
+          bal[i].radius += -5;
+        }
+      }
+      
+    //forloop end
+    }
+//animation end
 }
 
-shoppingSale();
+animate();
+
+setInterval(function() {
+  bal.push(new Ball());
+  bal.splice(0, 1);
+}, 400);
